34693ad0f22a581a3ceb9b7f6bf8a1636b239996
Saravanan C <sarvan.smartron@gmail.com>
2024-02-04 16:41:25 +0800

    Initial commit to configure variables

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-southeast-1"
 }
 
 ## Key to allow connection to our EC2 instance
@@ -23,12 +23,12 @@ variable "instance_type" {
 variable "alert_email_id" {
   description = "Email id to send alerts to "
   type        = string
-  default     = "you-name@some-domain.com"
+  default     = "sarvan.smartron@gmail.com"
 }
 
 ## Your repository url
 variable "repo_url" {
   description = "Repository url to clone into production machine"
   type        = string
-  default     = "https://github.com/josephmachado/data_engineering_project_template.git"
+  default     = "https://github.com/saravcha/data_engineering_project_template.git"
 }
